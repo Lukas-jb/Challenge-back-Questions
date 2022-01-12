d750f18f3b77308ee9eae8f3c7ac99e442dcb3fe
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2022-01-11 20:35:21 -0500

    Se deja ok el eliminar andswer

--- src/main/java/co/com/sofka/questions/model/AnswerDTO.java
+++ src/main/java/co/com/sofka/questions/model/AnswerDTO.java
@@ -30,6 +30,14 @@ public class AnswerDTO {
         this.answer = answer;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public Integer getPosition() {
         return Optional.ofNullable(position).orElse(1);
     }
